5fea4fc79b76db2777aad7c65921940a34818c1b
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-07 09:14:37 -0800

    finished complicated letter guessing game

--- pythonBasics/challenges.py
+++ pythonBasics/challenges.py
@@ -1,17 +1,26 @@
-def add(num1, num2):
-    #"try" to convert the numbers into floats...
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-    except ValueError:
-        #if we can't, we return None
-        return None
-    else:
-        #otherwise....
-        return num1+num2
+# def add(num1, num2):
+#     #"try" to convert the numbers into floats...
+#     try:
+#         num1 = float(num1)
+#         num2 = float(num2)
+#     except ValueError:
+#         #if we can't, we return None
+#         return None
+#     else:
+#         #otherwise....
+#         return num1+num2
 
 
-works = add(1, 2,)
-not_works = add('one', 'two')
+# works = add(1, 2,)
+# not_works = add('one', 'two')
 
-print(works, not_works)
+# print(works, not_works)
+
+import random
+def random_item(ite):
+    random_number = random.randint(0, (len(ite) - 1))
+    return ite[random_number]
+
+
+
+random_item(['hi', 'hi', 'hi'])
