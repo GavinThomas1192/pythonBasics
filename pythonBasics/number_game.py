d19bc57d8aa2ae5bd2416b2f98b9e67963fe9f87
GavinThomas1192 <gthomas1192@gmail.com>
2018-01-06 19:07:09 -0800

    guessing game with hints

--- pythonBasics/number_game.py
+++ pythonBasics/number_game.py
@@ -9,17 +9,28 @@ def rules():
     print("""
 Guess a number between 1 and 10!
 If you give up type 'quit'
+
 """)
 
 
+        # cached_anwser = "empty"
 def question():
     rules()
     while True:
-        answer = input("> ")
+        try:
+            answer = int(input("> "))
+        except ValueError:
+            print("{} isn't a number!".format(answer))
         if answer == "quit":
             break
+        elif answer < random_number:
+            print("Your last guess was {}. You were too low.".format(answer))
+            continue
+        elif answer > random_number:
+            print("Your last guess was {}. You were too High!".format(answer))
         elif int(answer) == random_number:
             print("CONGRATS YOU WON!!!")
+            print("The secret number was {}".format(random_number))
             break
         
         print('BUZZZZ WRONG')
